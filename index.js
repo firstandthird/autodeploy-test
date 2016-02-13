cb45a6f98a0d52a1c295a86e4269b947cfe78187
Greg Allen <me@jga.me>
2016-02-13 14:20:56 -0800

    output env

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const port = process.env.PORT || 5000;
 
 const server = http.createServer((request, response) => {
-  response.end('ok');
+  response.end(process.env);
 });
 
 server.listen(port, () => {
