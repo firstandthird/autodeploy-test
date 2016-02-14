f4d05f578e9a9d40bedb8a42364e99212c147609
Greg Allen <me@jga.me>
2016-02-13 16:51:53 -0800

    inc version

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const port = process.env.PORT || 5000;
 
 const server = http.createServer((request, response) => {
-  process.env.VERSION=1;
+  process.env.VERSION=2;
   response.end(JSON.stringify(process.env, null, '  '));
 });
 
