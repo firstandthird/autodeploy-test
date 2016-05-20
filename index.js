e8194983cc1a953f4d88c2e6519686dd6846ff2e
Greg Allen <greg@firstandthird.com>
2016-05-20 16:14:53 -0700

    upped version

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const port = process.env.PORT || 5000;
 
 const server = http.createServer((request, response) => {
-  process.env.VERSION=13;
+  process.env.VERSION=14;
   response.end(JSON.stringify(process.env, null, '  '));
 });
 
